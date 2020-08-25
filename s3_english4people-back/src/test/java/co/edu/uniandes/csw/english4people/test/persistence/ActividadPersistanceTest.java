1ea0b6b2cbd6219b2f4ae87058383079c6a68a10
Juan Diego Castellanos Bonilla <jd.castellanosb@uniandes.edu.co>
2020-08-25 00:26:57 -0500

    se realizo el test de persistencia para la actividad

---
+++ s3_english4people-back/src/test/java/co/edu/uniandes/csw/english4people/test/persistence/ActividadPersistanceTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.english4people.test.persistence;
+
+
+import co.edu.uniandes.csw.english4people.entities.ActividadEntity;
+import co.edu.uniandes.csw.english4people.persistence.ActividadPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Juan Diego Castellanos Bonilla
+ */
+@RunWith(Arquillian.class)
+public class ActividadPersistanceTest {
+    @Deployment
+    public static JavaArchive createDeployment(){
+    return ShrinkWrap.create(JavaArchive.class).addPackage(ActividadEntity.class.getPackage()).addPackage(ActividadPersistence.class.getPackage()).addAsManifestResource("META-INF/persistence.xml","persistence.xml").addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Inject
+    ActividadPersistence ac;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Test
+    public void createTest()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        ActividadEntity actividad = factory.manufacturePojo(ActividadEntity.class);
+        ActividadEntity result = ac.create(actividad);
+        
+        Assert.assertNotNull(result);
+        
+        ActividadEntity entity = em.find(ActividadEntity.class, result.getId());
+        
+        Assert.assertEquals(actividad.getTipo(), entity.getTipo());
+    }
+    private static void addAsManifestResource(String metainFpersistancexml, String persistancexml) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
