e6f9c4985150e9fc6a2f6b38ad066c3fe0c51bbe
Juan Diego Castellanos Bonilla <jd.castellanosb@uniandes.edu.co>
2020-08-25 00:25:19 -0500

    se realizo la entidad para la clase

---
+++ s3_english4people-back/src/main/java/co/edu/uniandes/csw/english4people/entities/ClaseEntity.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.english4people.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Juan Diego Castellanos Bonilla
+ */
+@Entity
+public class ClaseEntity extends BaseEntity implements Serializable {
+    
+    private Date fecha;
+    
+    private String tipoClase;
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the tipoClase
+     */
+    public String getTipoClase() {
+        return tipoClase;
+    }
+
+    /**
+     * @param tipoClase the tipoClase to set
+     */
+    public void setTipoClase(String tipoClase) {
+        this.tipoClase = tipoClase;
+    }
+}
