c388f200bade8fb898c40a3a84aed54b1de66180
Juan Diego Castellanos Bonilla <jd.castellanosb@uniandes.edu.co>
2020-08-25 00:25:04 -0500

    se realizo la entidad para la actividad

---
+++ s3_english4people-back/src/main/java/co/edu/uniandes/csw/english4people/entities/ActividadEntity.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.english4people.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Juan Diego Castellanos Bonilla 
+ */
+@Entity
+public class ActividadEntity extends BaseEntity implements Serializable {
+    
+   private Date fecha;
+   
+   private String tipo;
+   
+   private String descripcion;
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+   
+}
