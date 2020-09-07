2a184d2aeb23e802995e85963a4bb8f870b2d59c
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2020-09-07 12:31:37 -0500

    Se solucionó un issue en un método get de la entidad Contrato

--- s3_english4people-back/src/main/java/co/edu/uniandes/csw/english4people/entities/ContratoEntity.java
+++ s3_english4people-back/src/main/java/co/edu/uniandes/csw/english4people/entities/ContratoEntity.java
@@ -9,9 +9,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import uk.co.jemos.podam.common.PodamExclude;
 
 /**
@@ -20,6 +18,20 @@ import uk.co.jemos.podam.common.PodamExclude;
  */
 @Entity
 public class ContratoEntity extends BaseEntity implements Serializable{
+
+    /**
+     * @return the horarios
+     */
+    public List<HorarioEntity> getHorarios() {
+        return horarios;
+    }
+
+    /**
+     * @param horarios the horarios to set
+     */
+    public void setHorarios(List<HorarioEntity> horarios) {
+        this.horarios = horarios;
+    }
 
     /**
      * @return the profesor
@@ -53,14 +65,7 @@ public class ContratoEntity extends BaseEntity implements Serializable{
      * @return the horario
      */
     public List<HorarioEntity> getHorario() {
-        return horarios;
-    }
-
-    /**
-     * @param horario the horario to set
-     */
-    public void setHorario(List<HorarioEntity> horarios) {
-        this.horarios = horarios;
+        return getHorarios();
     }
     
     private Integer numeroHoras;
